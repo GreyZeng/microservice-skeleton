dc7aedf5f7ce846e9c4e6a268a8cec5f19c6d815
GreyZeng <410486047@qq.com>
2022-11-21 13:24:57 +0800

    update

--- README.md
+++
@@ -1,5 +0,0 @@
-参考资料
-
-[重新定义Spring Cloud实战](https://book.douban.com/subject/30338647/)
-
-[spring-cloud-code](https://github.com/SpringCloud/spring-cloud-code)
--- skeleton-register-server/src/main/java/cn/springcloud/book/eureka/RegisterServerApplication.java
+++ skeleton-register-server/src/main/java/cn/springcloud/book/eureka/RegisterServerApplication.java
@@ -5,13 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * eureka server
+ * 注册中心
  */
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServerApplication {
+public class RegisterServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class, args);
+        SpringApplication.run(RegisterServerApplication.class, args);
     }
 }
