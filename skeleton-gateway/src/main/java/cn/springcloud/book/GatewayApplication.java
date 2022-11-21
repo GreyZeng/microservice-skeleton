aefd6f44127a21c25748711bf6ba1cde3a4d9a37
GreyZeng <410486047@qq.com>
2022-11-21 22:42:50 +0800

    update

---
+++ skeleton-gateway/src/main/java/cn/springcloud/book/GatewayApplication.java
@@ -0,0 +1,20 @@
+package cn.springcloud.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableDiscoveryClient
+//@EnableZuulProxy
+//@EnableCircuitBreaker
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+//    @Bean
+//    public AuthFilter preRequestFilter() {
+//        return new AuthFilter();
+//    }
+}
