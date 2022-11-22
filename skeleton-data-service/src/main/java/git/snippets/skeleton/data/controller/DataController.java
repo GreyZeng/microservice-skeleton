964c025d1de326295eceb283fbcf18a7126e14e7
GreyZeng <410486047@qq.com>
2022-11-22 22:42:23 +0800

    update

---
+++ skeleton-data-service/src/main/java/git/snippets/skeleton/data/controller/DataController.java
@@ -0,0 +1,34 @@
+package git.snippets.skeleton.data.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DataController {
+
+//    private final DataConfig dataConfig;
+//
+//    public DataController(DataConfig dataConfig) {
+//        this.dataConfig = dataConfig;
+//    }
+//
+//    @GetMapping("/getContextUserId")
+//    public String getContextUserId() {
+//        return UserContextHolder.currentUser().getUserId();
+//    }
+//
+//    @GetMapping("/getDefaultUser")
+//    public String getDefaultUser() {
+//        return dataConfig.getDefaultUser();
+//    }
+//
+//    @GetMapping("/getProviderData")
+//    public List<String> getProviderData() {
+//        List<String> provider = new ArrayList<String>();
+//        provider.add("Beijing Company");
+//        provider.add("Shanghai Company");
+//        provider.add("Shenzhen Company");
+//        return provider;
+//    }
+
+
+}
--- skeleton-user-service/src/main/java/git/snippets/skeleton/user/service/dataservice/DataService.java
+++ skeleton-user-service/src/main/java/git/snippets/skeleton/user/service/dataservice/DataService.java
@@ -16,3 +16,23 @@
 //    String getContextUserId();
 //
 //}
+package git.snippets.skeleton.user.service.dataservice;
+
+
+import git.snippets.skeleton.common.vo.User;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+@FeignClient(name = "sc-data-service")
+public interface DataService {
+    @RequestMapping(value = "/user/add", method = GET)
+    String addUser(User user);
+
+    @RequestMapping(value = "/user/update", method = POST)
+    String updateUser(@RequestBody User user);
+}
+
