0fad43623c713af4a120608026bfe15f69bf25ae
GreyZeng <410486047@qq.com>
2022-12-17 19:53:55 +0800

    revert to f version

---
+++ README.md
@@ -0,0 +1,5 @@
+参考资料
+
+[重新定义Spring Cloud实战](https://book.douban.com/subject/30338647/)
+
+[spring-cloud-code](https://github.com/SpringCloud/spring-cloud-code)
--- skeleton-common/src/main/java/git/snippets/skeleton/common/context/HystrixThreadLocal.java
+++
@@ -1,5 +0,0 @@
-//package git.snippets.skeleton.common.context;
-//
-//public class HystrixThreadLocal {
-//    public static ThreadLocal<String> threadLocal = new ThreadLocal<>();
-//}
